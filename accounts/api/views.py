4342838024745c77842a2e479c11aba0ee8a20da
Hang Yuan <hyuan13@illinois.edu>
2021-04-17 17:32:41 -0500

    add signup feature for user

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,13 +1,41 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from accounts.api.serializers import UserSerializer, SignupSerializer
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login
+)
+from django.contrib.auth.models import User
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+    permission_classes = (AllowAny,)
+    serializer_class = SignupSerializer
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=400)
+
+        user=serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data,
+        })
